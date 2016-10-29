bf5095b591110006009f3587e463b33cea1cf439
Diego Giacomelli <giacomelli@gmail.com>
2016-10-29 18:59:09 -0200

    Added debug window to generate FileId based on Type name.

--- src/Infrastructure.Framework/Serialization/FileIdUtil.cs
+++ src/Infrastructure.Framework/Serialization/FileIdUtil.cs
@@ -7,32 +7,52 @@ namespace Giacomelli.Unity.Metadata.Infrastructure.Framework.Serialization
 	/// <summary>
 	/// File identifier util.
 	/// </summary>
-    public static class FileIdUtil
-    {
+	public static class FileIdUtil
+	{
 		/// <summary>
 		/// Get the file id from specified type.
 		/// </summary>
 		/// <returns>The type.</returns>
 		/// <param name="type">Type.</param>
-        public static int FromType(Type type)
-        {
-            // http://forum.unity3d.com/threads/yaml-fileid-hash-function-for-dll-scripts.252075/#post-1695479
-            string toBeHashed = "s\0\0\0" + type.Namespace + type.Name;
+		public static int FromType(Type type)
+		{
+			if (type.Assembly.GetName().Name.Equals("Assembly-CSharp"))
+			{
+				return 11500000;
+			}
 
-            using (HashAlgorithm hash = new MD4())
-            {
-                byte[] hashed = hash.ComputeHash(Encoding.UTF8.GetBytes(toBeHashed));
+			return FromType(type.Namespace + type.Name);
+		}
 
-                int result = 0;
+		/// <summary>
+		/// Get the file id from specified type.
+		/// </summary>
+		/// <returns>The type.</returns>
+		/// <param name="type">Type.</param>
+		public static int FromType(string typeName)
+		{
+			if (string.IsNullOrEmpty(typeName))
+			{
+				throw new ArgumentNullException("typeName");
+			}
 
-                for (int i = 3; i >= 0; --i)
-                {
-                    result <<= 8;
-                    result |= hashed[i];
-                }
+			// http://forum.unity3d.com/threads/yaml-fileid-hash-function-for-dll-scripts.252075/#post-1695479
+			string toBeHashed = "s\0\0\0" + typeName;
 
-                return result;
-            }
-        }
-    }
+			using (HashAlgorithm hash = new MD4())
+			{
+				byte[] hashed = hash.ComputeHash(Encoding.UTF8.GetBytes(toBeHashed));
+
+				int result = 0;
+
+				for (int i = 3; i >= 0; --i)
+				{
+					result <<= 8;
+					result |= hashed[i];
+				}
+
+				return result;
+			}
+		}
+	}
 }
